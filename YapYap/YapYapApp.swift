09947895d58810314ec4ab6a13af506778fef0c5
Harry Hou <harrygoodwork@gmail.com>
2024-06-04 00:16:12 +0800

    Initial Commit

---
+++ YapYap/YapYapApp.swift
@@ -0,0 +1,17 @@
+//
+//  YapYapApp.swift
+//  YapYap
+//
+//  Created by harryhou on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct YapYapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
